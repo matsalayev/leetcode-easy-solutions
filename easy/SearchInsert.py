2d04ebd52b70285a7d9b6c51ed8a5be0cdef2f87
matsalayev <matsalayev@outlook.com>
2025-09-06 21:38:28 +0500

    https://leetcode.com/problems/search-insert-position/

---
+++ easy/SearchInsert.py
@@ -0,0 +1,22 @@
+"""
+Runtime: 0ms
+Beats: 100.00%
+
+Memory: 12.92MB
+Beats: 57.53%
+"""
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
